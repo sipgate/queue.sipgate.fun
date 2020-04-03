604f4a74114f41cb0ccafc996dc319325906dca7
Benjamin Kluck <kluck@sipgate.de>
2020-04-03 12:58:09 +0200

    ✨ Show queues

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from "react";
 import "./App.css";
-import { useAuth } from "./firebase";
-import logo from "./logo.svg";
+import { useAuth, useQueues } from "./firebase";
 
 function App() {
   const user = useAuth();
+  const queues = useQueues();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Queues</h1>
+      {queues.map(queue => (
+        <div>{queue.name}</div>
+      ))}
     </div>
   );
 }
